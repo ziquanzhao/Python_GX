235c87a9065afeb4fdf567dde6bf04ab33a8aea0
zzq <2939818719@qq.com>
2022-08-22 15:06:54 +0800

    Initial commit

--- GWAS/Trimmomatic.py
+++ GWAS/Trimmomatic.py
@@ -14,7 +14,6 @@ with open('trimmomatic.sh', 'a') as trim:
         trim.write(f'java -jar /mnt/storage/zhaoziquan/GWAS/software/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 60 -phred33 /mnt/data/project/unknown/reseq/bypy/cleandata/{x}_R1.fq.gz /mnt/data/project/unknown/reseq/bypy/cleandata/{x}_R2.fq.gz -baseout {x}.fq.gz ILLUMINACLIP:/mnt/storage/zhaoziquan/GWAS/software/Trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:30:10:2:true SLIDINGWINDOW:4:15 LEADING:3 TRAILING:3 MINLEN:36 HEADCROP:5\n')
 
 os.mkdir('zzq_chean_data')
-os.system('mv trimmomatic.sh ./zzq_clean_data/')
 print('当前目录下有一个zzq_clean_data目录，运行其文件夹下的shell脚本即可')
 
 
