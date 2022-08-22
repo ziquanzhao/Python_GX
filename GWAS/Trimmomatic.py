06398a1ffedb696f3babcdc7bc4ab271a021b0f3
zzq <2939818719@qq.com>
2022-08-22 15:06:26 +0800

    Initial commit

--- GWAS/Trimmomatic.py
+++ GWAS/Trimmomatic.py
@@ -11,7 +11,7 @@ with open('trimmomatic.sh', 'a') as trim:
     trim.write('#!/bin/bash\n')
     for i in list:
         x = i.replace('\n', '')
-        trim.write(f'java -jar /mnt/storage/zhaoziquan/GWAS/software/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 60 -phred33 {x}_R1.fq.gz {x}_R2.fq.gz -baseout {x}.fq.gz ILLUMINACLIP:/mnt/storage/zhaoziquan/GWAS/software/Trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:30:10:2:true SLIDINGWINDOW:4:15 LEADING:3 TRAILING:3 MINLEN:36 HEADCROP:5\n')
+        trim.write(f'java -jar /mnt/storage/zhaoziquan/GWAS/software/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 60 -phred33 /mnt/data/project/unknown/reseq/bypy/cleandata/{x}_R1.fq.gz /mnt/data/project/unknown/reseq/bypy/cleandata/{x}_R2.fq.gz -baseout {x}.fq.gz ILLUMINACLIP:/mnt/storage/zhaoziquan/GWAS/software/Trimmomatic-0.39/adapters/TruSeq3-PE.fa:2:30:10:2:true SLIDINGWINDOW:4:15 LEADING:3 TRAILING:3 MINLEN:36 HEADCROP:5\n')
 
 os.mkdir('zzq_chean_data')
 os.system('mv trimmomatic.sh ./zzq_clean_data/')
